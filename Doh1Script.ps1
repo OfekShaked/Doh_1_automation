acf2455a2d1bd51dd87875c9aa8461d2e71ddcc9
OfekShaked <68245887+OfekShaked@users.noreply.github.com>
2020-07-13 21:10:53 +0300

    Add files via upload

---
+++ Doh1Script.ps1
@@ -0,0 +1,3 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+Start-Process -FilePath "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "file:///C:/autoD/Doh_1_fill.html?direct=1"
+end
