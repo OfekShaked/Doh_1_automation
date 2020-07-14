5e54878e9f24d04c80406afac651d3d8e32845c9
OfekShaked <68245887+OfekShaked@users.noreply.github.com>
2020-07-14 09:19:43 +0300

    Add files via upload

---
+++ TestForAutoSuper.ps1
@@ -0,0 +1,22 @@
+﻿#Start-Process 
+$ie = New-Object -com InternetExplorer.Application
+#"InternetExplorer.Application"
+$ie.Navigate("https://one.prat.idf.il/hp")
+$ie.visible = $true
+#Create a FailureCount variable
+sleep 30
+
+$app = New-Object -ComObject Shell.Application
+$RandomPage = $app.Windows() | where {$_.LocationURL -like     "https://one.prat.idf.il/hp"}
+
+#Check to see if page has opened
+if( $RandomPage.visible -eq $true)
+{
+Echo "Random Page is available"
+sleep 10
+$RandomPage.document.documentElement.getElementsByClassName('icon').Click()
+}
+else
+{
+echo "Page not loaded"
+}
